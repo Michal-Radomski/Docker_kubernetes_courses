c50a9b2fe67f30789dc3dae55832dac611a55aa5
Michal Radomski <michalra@op.pl>
2024-02-18 21:09:02 +0100

    Docker & Kubernetes course - commit 06

---
+++ 01 Docker&Kubernetes/01/server.ts
@@ -0,0 +1,51 @@
+import http from "http";
+import path from "path";
+import * as dotenv from "dotenv";
+dotenv.config();
+import express, { Express, Request, Response } from "express";
+import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
+
+//* The server
+const app: Express = express();
+
+const corsOptions = {
+  origin: true,
+  methods: ["GET", "POST"],
+  preflightContinue: false,
+  optionsSuccessStatus: 200,
+  credentials: true,
+};
+
+//* Middlewares
+app.use(cors(corsOptions));
+app.use(morgan("combined"));
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+    crossOriginResourcePolicy: false,
+    crossOriginEmbedderPolicy: false,
+    crossOriginOpenerPolicy: false,
+  })
+);
+
+//* Favicon
+app.get("/favicon.ico", (_req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname + "/favicon.svg"));
+});
+//* Test route
+app.get("/", (req: Request, res: Response) => {
+  console.log("req.ip:", req.ip);
+  res.send("<p style='color:gray;'>API is running</p>");
+});
+
+//* Port
+const portHTTP = (process.env.PORT || 5000) as number;
+
+const httpServer = http.createServer(app);
+httpServer.listen({ port: portHTTP }, () => {
+  console.log(`Server is listening at http://localhost:${portHTTP}`);
+  // For testing only
+  console.log("Current Time:", new Date().toLocaleTimeString());
+});
