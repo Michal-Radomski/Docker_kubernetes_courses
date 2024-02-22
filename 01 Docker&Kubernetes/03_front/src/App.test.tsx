2753a98e3cf37b5064619218685fda7333d04bd0
Michal Radomski <michalra@op.pl>
2024-02-22 03:13:14 +0100

    Docker & Kubernetes course - commit 17

--- 01 Docker&Kubernetes/03_front/src/App.test.tsx
+++ 01 Docker&Kubernetes/03_front/src/App.test.tsx
@@ -3,7 +3,21 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("renders learn react link", () => {
-  render(<App />);
+  render(
+    <React.Fragment>
+      <App />
+    </React.Fragment>
+  );
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders learn react link2", () => {
+  render(
+    <React.Fragment>
+      <App />
+    </React.Fragment>
+  );
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
--- 01 Docker&Kubernetes/03_front/src/App.tsx
+++ 01 Docker&Kubernetes/03_front/src/App.tsx
@@ -12,10 +12,16 @@ function App(): JSX.Element {
           <p>
             To test, change the file...
             <br />
+            To test, change the file... again...
+            <br />
+            To test, change the file... and again...
+            <br />
+            To test, change the file... and again... and again...
+            <br />
             To get started {"=>"} try to edit <code>src/App.tsx</code> and save to reload.
           </p>
           <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-            Learn Docker with React and Redux and TS
+            Learn React
           </a>
         </header>
       </div>
